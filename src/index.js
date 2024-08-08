15c5431ae368bdd0060f73608c5769fd922fb605
ppotapkin_wh <pavel.potapkin@grandparade.co.uk>
2024-08-08 21:26:48 +0200

    add merge call

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const httpClient = require('@actions/http-client');
 
-try {
+async function run() {
+    try {
 
-    const fromBranch = core.getInput('from_branch');
-    const toBranch = core.getInput('to_branch');
-    const headToMerge = core.getInput('head_to_merge');
+        const token  = github.context.payload.token;
+        const octokit = github.getOctokit(token);
+        const repo = github.context.payload.repository;
+        const owner = github.context.payload.owner;
+        const fromBranch = core.getInput('from_branch', { required: true });
+        const toBranch = core.getInput('target_branch', { required: true });
 
-    core.info(fromBranch);
-    core.info(toBranch);
-    core.info(headToMerge);
+        await octokit.request(`POST /repos/${owner}/{${repo}/merges`, {
+            owner: owner,
+            repo: repo,
+            base: fromBranch,
+            head: toBranch,
+            commit_message: `Merge ${fromBranch}!`
+        });
 
-} catch (error) {
-    core.setFailed(error.message);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
